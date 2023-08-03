cbfe761f85f7b5bf1a28c35546a2fc7a8a383340
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-08-03 22:54:44 +0300

    Solution Task 2

--- src/app/task2/Main.java
+++ src/app/task2/Main.java
@@ -1,7 +1,33 @@
 package app.task2;
 
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+        getOutput(filterData(getInitData()));
+    }
+    private static Map<String, Double> getInitData() {
+        return Map.of(
+                "onion", 1.25,
+                "potato", 1.05,
+                "tomato", 3.05,
+                "cucumber", 1.4,
+                "red pepper", 2.50,
+                "carrot", 1.70,
+                "greens", 1.50);
+    }
 
+    private static void getOutput(Stream<String> products) {
+        System.out.println("Products with price lower than 2.00:");
+        AtomicInteger counter = new AtomicInteger(1);
+        products.forEach(name -> System.out.println(counter.getAndIncrement() + ") " + name));
+    }
+
+    private static Stream<String> filterData(Map<String, Double> products) {
+        return products.entrySet().stream()
+                .filter(product -> product.getValue() <= 2.0)
+                .map(Map.Entry::getKey);
     }
 }
